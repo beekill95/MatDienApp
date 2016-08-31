c169f4cd62cc2c9c45eee515ea2f217f50d9afa1
beekill <nnn_bikiu0707@yahoo.com>
2016-08-31 10:56:27 +0700

    Design basic layout for ChangePasswordActivity

---
+++ app/src/main/java/com/example/beekill/matdienapp/activities/ChangePasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.beekill.matdienapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.beekill.matdienapp.R;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
