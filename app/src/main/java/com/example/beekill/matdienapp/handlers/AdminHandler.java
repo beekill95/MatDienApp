fd4386d5cf1d8088256f1eb2310a3134aed31453
beekill <nnn_bikiu0707@yahoo.com>
2016-10-20 20:30:20 +0700

    Delete package handler

--- app/src/main/java/com/example/beekill/matdienapp/handlers/AdminHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.beekill.matdienapp.handlers;
-
-import com.example.beekill.matdienapp.protocol.Response;
-/**
- * Created by beekill on 8/13/16.
- */
-public interface AdminHandler {
-    void handleInAdmin(Response resposne);
-}
