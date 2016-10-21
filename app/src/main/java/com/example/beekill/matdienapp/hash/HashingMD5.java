5734ab8c4e278932a3df5e7fa3b9bc846750c982
beekill <nnn_bikiu0707@yahoo.com>
2016-10-21 10:38:40 +0700

    Implement MD5 hash

--- app/src/main/java/com/example/beekill/matdienapp/hash/HashingMD5.java
+++ app/src/main/java/com/example/beekill/matdienapp/hash/HashingMD5.java
@@ -3,14 +3,31 @@ package com.example.beekill.matdienapp.hash;
 /**
  * Created by beekill on 10/2/16.
  */
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashingMD5 extends Hashing {
     @Override
     public void hash(String password, String[] hashedPassword) {
-
+        hashedPassword[0] = "";
+        hashedPassword[1] = hash(password, "");
     }
 
     @Override
     public String hash(String password, String salt) {
+        try {
+            byte[] bytesOfPassword = password.getBytes("UTF-8");
+
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashedBytes = md.digest(bytesOfPassword);
+
+            return Base64.encodeToString(hashedBytes, Base64.DEFAULT);
+        } catch (UnsupportedEncodingException e) { }
+        catch (NoSuchAlgorithmException e) { }
+
         return null;
     }
 }
